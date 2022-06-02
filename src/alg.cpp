198ef73984cc474652dc76b4168eaf2df7c3e36f
Дреняев Максим <drenyaev.max@gmail.com>
2022-06-02 13:37:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,15 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include  <vector>
 #include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+  std::vector<char> out;
+  std::string perm = tree.get(n-1);
+
+  for (int i = 0; i < perm.size(); i++) {
+    out.push_back(perm[i]);
+  }
+  return out;
 }
